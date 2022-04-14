959027ac6df1918e1127732562b5b61b7643e5e8
kurooooon <b1006110@gmail.com>
2022-04-14 18:01:00 +0900

    test paralell

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { babel } from '@rollup/plugin-babel';
 const config = {
   input: 'src/app.js',
   output: {
-	file: 'dist/bundle.js',
+	file: 'build/bundle.js',
     format: 'iife'
   },
   plugins: [babel({ babelHelpers: 'bundled' })]
